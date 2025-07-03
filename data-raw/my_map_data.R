58009bd13f0087fc40eaabd329fe7739e14dda54
Guido Philipp Emmanuel Biele <Guido.Biele@fhi.no>
2025-07-03 10:05:59 +0200

    updates

--- data-raw/my_map_data.R
+++ data-raw/my_map_data.R
@@ -4,18 +4,19 @@ library(MapNorway)
 
 MapBydeler = load_bydeler("data-raw/bydel2020_mednavn.gml","data-raw/fylker.geojson")
 
+MapKommuner = load_kommuner("data-raw/kommune.gml","data-raw/fylker.geojson")
+
 MapFylker =
   st_read("data-raw/fylker.geojson") %>%
-  st_transform(crs = st_crs(kommuner)) %>%
+  st_transform(crs = st_crs(MapKommuner)) %>%
   ms_simplify(keep = 0.0005, keep_shapes = TRUE)
+
 MapFylker = MapFylker[, c("navn","nummer")]
 
 MapFylkerBydeler = create_inset_map(MapFylker,MapBydeler)
 
 
-MapKommuner = load_kommuner("data-raw/kommune.gml","data-raw/fylker.geojson")
 
 usethis::use_data(MapKommuner)
-usethis::use_data(MapBydeler)
+usethis::use_data(MapFylker)
 usethis::use_data(MapFylkerBydeler)
-
